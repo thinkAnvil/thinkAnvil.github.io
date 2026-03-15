84666c79dcc619eac6b7d88bf43a86c4623ba65c
uijin <uijin@uijinui-MacBookAir.local>
2026-03-15 22:28:55 +0900

    first blog

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773388457',resources: [
+    cacheName: 'chirpy-1773408819',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
